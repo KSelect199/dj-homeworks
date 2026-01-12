cccac828fd2963f85549ccd7b54184411fc0b8f4
KSelect199 <146189488+KSelect199@users.noreply.github.com>
2026-01-12 17:45:03 +0500

    Update models.py

--- databases/work_with_database/phones/models.py
+++ databases/work_with_database/phones/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
-
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name="Название")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    image = models.URLField(max_length=500, verbose_name="Изображение")
+    release_date = models.DateField(verbose_name="Дата релиза")
+    lte_exists = models.BooleanField(default=False, verbose_name="Поддержка LTE")
+    slug = models.SlugField(unique=True, max_length=255, blank=True, verbose_name="Слаг")
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Телефон"
+        verbose_name_plural = "Телефоны"
